9816c4d07e42e729d352db454ac6b43fc1e75c39
Mikhail Viktorov <the-rayn@yandex.ru>
2024-05-22 00:39:17 +0300

    feat: add database tests

---
+++ src/test/resources/userDbStorageTest.sql
@@ -0,0 +1,5 @@
+merge into users (id, birthday, name, login, email)
+    values (0, '1990-01-01', 'Mikhail', 'Mikhail_login', 'mikhail@email.ru');
+
+merge into users (id, birthday, name, login, email)
+    values (1, '1990-01-01', 'Mikhail2', 'Mikhail_login2', 'mikhail2@email.ru');
